2fa0b753ae197ade72cd1c01b28476e2cdf299e7
WessonX <wessonx@163.com>
2023-02-22 20:45:15 +0800

    mod:relay不要主动断开与peer节点的连接

--- RosAgent/rosAgent.go
+++ RosAgent/rosAgent.go
@@ -122,14 +122,14 @@ func main() {
 	/*
 		与9090端口的ros_server建立websocket连接
 	*/
-	dialer := websocket.Dialer{}
-	rosConn, _, err := dialer.Dial("ws://127.0.0.1:9090", nil)
-	if err != nil {
-		panic("连接ros_server失败" + err.Error())
-	}
-	fmt.Println("连接ros_server成功")
-	roshandler = &RosHandler{RosConn: rosConn}
-	go roshandler.rosRead()
+	// dialer := websocket.Dialer{}
+	// rosConn, _, err := dialer.Dial("ws://127.0.0.1:9090", nil)
+	// if err != nil {
+	// 	panic("连接ros_server失败" + err.Error())
+	// }
+	// fmt.Println("连接ros_server成功")
+	// roshandler = &RosHandler{RosConn: rosConn}
+	// go roshandler.rosRead()
 
 	/*
 		与对端节点建立p2p连接
@@ -146,7 +146,7 @@ func main() {
 
 	// 若失败，则断开与ros_server的连接；浏览器会直接通过frp连接ros_server
 	if !isSuccess {
-		rosConn.Close()
+		// rosConn.Close()
 	} else {
 		// 若成功，则从rosAgent的channelDate中读取数据，发送给ros_server
 		go func() {
